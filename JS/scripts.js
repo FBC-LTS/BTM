9e74e7788ca880458b4cc119ea4f03d92ef16344
FBC-LTS <101365427+FBC-LTS@users.noreply.github.com>
2022-06-11 16:56:04 -0300

    fix
    fix trailer

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,10 @@
+const btnTrailer = document.getElementById("b-trailer");
+const clsIco = document.getElementById("cls-ico");
+btnTrailer.addEventListener("click", toggleTrailer);
+clsIco.addEventListener("click", toggleTrailer);
+
 function toggleTrailer(){
     var trailer= document.querySelector(".trailer");
-    trailer.classList.toggle('active')
+    trailer.classList.toggle('active');
 }
 
