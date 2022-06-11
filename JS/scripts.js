85046e0452a841cc872855158b645dc72d3eda81
FBC-LTS <101365427+FBC-LTS@users.noreply.github.com>
2022-06-11 17:03:19 -0300

    fix

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,15 @@
 const btnTrailer = document.getElementById("b-trailer");
 const clsIco = document.getElementById("cls-ico");
-btnTrailer.addEventListener("click", toggleTrailer);
-clsIco.addEventListener("click", toggleTrailer);
+btnTrailer.addEventListener("click", openTrailer);
+clsIco.addEventListener("click", closeTrailer);
 
-function toggleTrailer(){
-    var trailer= document.querySelector(".trailer");
-    trailer.classList.toggle('active');
+function openTrailer(){
+    var trailer= document.getElementById("trailer");
+    trailer.classList.add("active");
+    
 }
-
+function closeTrailer(){
+    var trailer= document.getElementById("trailer");
+    trailer.classList.remove("active");
+    
+}
